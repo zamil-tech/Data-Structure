5fb8f6893b8c23a259eb9851fc8e19f95859988b
Muzamil <90958923+zamil623@users.noreply.github.com>
2021-11-01 18:59:45 +0000

    Add files via upload

---
+++ subseq by recursion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void subseq(string s,string ans){
+	if(s.length()==0){
+		return;
+	}
+	char ch=s[0];
+	string ros=s.substr(1);
+	subseq(ros,ans);
+	subseq(ros,ans+ch);
+}
+int main(){
+	subseq("ABC","");
+	cout<<endl;
+}
